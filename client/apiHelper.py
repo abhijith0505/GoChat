1d7f55cb1879292b06f7b7240fe2885616068b9f
abhijith0505 <abhijith4m0505@gmail.com>
2016-12-16 15:45:11 +0530

    password hashing

--- client/apiHelper.py
+++ client/apiHelper.py
@@ -1,5 +1,12 @@
 import requests, json
-import variables
+import variables, pymongo
+from pymongo import MongoClient
+from bson.json_util import dumps
+
+client = MongoClient()
+db = client['gochat']
+user = db['user']
+user.ensure_index('username',unique=True)
 
 def checkUser(username):
     url = variables.AWSEndPoint + "/checkUser"
@@ -15,4 +22,17 @@ def registerUser(username, password):
     json_data = json.dumps(data)
     headers = {'Content-type': 'application/json'}
     response = requests.post(url, data=json_data, headers=headers)
+    try:
+        db.user.insert_one({'username':username, 'password': hash_pass(password)})
+    except:
+        print "nononono"
+
     return response.text
+
+def hash_pass(password):
+    # used to hash the password similar to how MySQL hashes passwords with the password() function.
+    import hashlib
+    hash_password = hashlib.sha1(password.encode('utf-8')).digest()
+    hash_password = hashlib.sha1(hash_password).hexdigest()
+    hash_password = '*' + hash_password.upper()
+    return hash_password
--- client/goChat.py
+++ client/goChat.py
@@ -7,6 +7,12 @@ parser = argparse.ArgumentParser(description='This is terminal chat application'
 parser.add_argument('-r', '--register', action='store_true', dest='register',
                     help='Register user')
 
+parser.add_argument('-li', '--login', action='store_true', dest='login',
+                    help='Log into account')
+
+parser.add_argument('-lo', '--logout', action='store_true', dest='logout',
+                    help='Log out')
+
 parser.add_argument('--version', '-v', action='version', version='%(prog)s 1.0')
 
 if len(sys.argv) < 2:
@@ -16,13 +22,29 @@ if len(sys.argv) < 2:
 args = parser.parse_args()
 
 register = args.register
+login = args.login
+logout = args.logout
 
-if args.register:
+if register:
      username = raw_input("Username:")
      password = getpass.getpass("Password:")
+     while not username:
+     	print "Please choose a username"
+     	username = raw_input("Username:")
+
+     while not password:
+     	print "Please choose a password"
+     	password = getpass.getpass("Password:")
+
      response = apiHelper.registerUser(username,password)
      if response == "exists":
           print "User exists! Try again"
           sys.exit(1)
      else:
           print "User created!"
+          print "Please login"
+          parser.print_usage()
+
+if login:
+	username = raw_input("Username:")
+	password = getpass.getpass("Password:")
